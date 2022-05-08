59fe968248a01b70ff598f0e5e0bf52f77df638a
Ilio Catallo <ilio.catallo@gmail.com>
2022-05-08 17:06:22 +0200

    Improve wording

--- test/map.test.ts
+++ test/map.test.ts
@@ -2,7 +2,7 @@ import { test } from 'uvu';
 import * as assert from 'uvu/assert';
 import { map } from '../src/map';
 
-test(`map values satisfying the type guard`, () => {
+test(`map transforms values satisfying the type guard`, () => {
   const input: string | number = 'hello';
 
   const res = map(isString, (s: string) => s.toUpperCase(), input);
@@ -10,7 +10,7 @@ test(`map values satisfying the type guard`, () => {
   assert.is(res, 'HELLO');
 });
 
-test('ignore values that do not satisfy the type guard', () => {
+test('map ignores values that do not satisfy the type guard', () => {
   const input: string | number = 42;
 
   const res = map(isString, (s: string) => s.toUpperCase(), input);
