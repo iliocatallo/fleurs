1057f67ce1df2c98c9f550c84e6dc280b3e2e169
Ilio Catallo <ilio.catallo@gmail.com>
2022-05-08 16:52:30 +0200

    Run fmt script

--- src/map.ts
+++ src/map.ts
@@ -1,8 +1,8 @@
 export function map<T, R, O>(guard: Guard<T>, mapper: Mapper<T, O>, x: T | R): O | R {
-	if (guard(x)) {
-		return mapper(x);
-	}
-	return x;
+  if (guard(x)) {
+    return mapper(x);
+  }
+  return x;
 }
 
 type Guard<T> = (x: any) => x is T;
--- test/map.test.ts
+++ test/map.test.ts
@@ -3,21 +3,21 @@ import * as assert from 'uvu/assert';
 import { map } from '../src/map';
 
 test(`map values satisfying the type guard`, () => {
-	const input: string | number = 'hello';
+  const input: string | number = 'hello';
 
-	const res = map(isString, (s: string) => s.toUpperCase(), input);
+  const res = map(isString, (s: string) => s.toUpperCase(), input);
 
-	assert.is(res, 'HELLO');
+  assert.is(res, 'HELLO');
 });
 
 test('ignore values that do not satisfy the type guard', () => {
-	const input: string | number = 42;
+  const input: string | number = 42;
 
-	const res = map(isString, (s: string) => s.toUpperCase(), input);
+  const res = map(isString, (s: string) => s.toUpperCase(), input);
 
-	assert.is(res, 42);
-})
+  assert.is(res, 42);
+});
 
-const isString = (x: any): x is string => typeof x == 'string'
+const isString = (x: any): x is string => typeof x == 'string';
 
 test.run();
