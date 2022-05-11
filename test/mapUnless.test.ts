c332ada6ef704e687e63405fb5c1833e90c65e9f
Ilio Catallo <ilio.catallo@gmail.com>
2022-05-11 19:09:45 +0200

    Improve wording

--- test/mapUnless.test.ts
+++ test/mapUnless.test.ts
@@ -3,7 +3,7 @@ import * as assert from 'uvu/assert';
 import { mapUnless } from '../src/map';
 import { isString } from './isString';
 
-test(`mapUnless transforms values that do not satisfy the type guard`, () => {
+test(`values that do not satisfy the the type guard get transformed`, () => {
   const input: string | number = 20;
 
   const res = mapUnless(isString, (x: number) => x + 2, input);
@@ -11,7 +11,7 @@ test(`mapUnless transforms values that do not satisfy the type guard`, () => {
   assert.is(res, 22);
 });
 
-test(`mapUnles ignores values that satisfy the type guard`, () => {
+test(`values that satisfy the type guard are returned as-is`, () => {
   const input: string | number = 'hello';
 
   const res = mapUnless(isString, (x: number) => x + 2, input);
