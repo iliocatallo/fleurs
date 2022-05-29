623b79f9baef84e0fab0450a1755d9074686c410
Ilio Catallo <ilio.catallo@gmail.com>
2022-05-29 10:05:38 +0200

    Reorder type variables

--- src/map.ts
+++ src/map.ts
@@ -1,13 +1,13 @@
 import { Guard } from './Guard';
 
-export function mapIf<U, H extends U, O>(guard: Guard<H>, mapper: Mapper<H, O>, x: U): Exclude<U, H> | O {
+export function mapIf<U, O, H extends U = U>(guard: Guard<H>, mapper: Mapper<H, O>, x: U): Exclude<U, H> | O {
   if (guard(x)) {
     return mapper(x);
   }
   return x as Exclude<U, H>;
 }
 
-export function mapUnless<U, H extends U, O>(guard: Guard<H>, mapper: Mapper<Exclude<U, H>, O>, x: U): H | O {
+export function mapUnless<U, O, H extends U = U>(guard: Guard<H>, mapper: Mapper<Exclude<U, H>, O>, x: U): H | O {
   if (guard(x)) {
     return x;
   }
